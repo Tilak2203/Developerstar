92d1237689c69f3224ede4547b810d1a5df831c0
Tilak <tilak.tjilka@gmail.com>
2022-12-23 14:43:40 +0530

    main function

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,24 @@
 
 </script>
 
+
 <main>
-  <h1>BOT</h1>
+  <h1 class="heading">Image Compressor</h1>
+
+  <div class="main_content">
+      <input type="file" id="my_image">
+
+      <span>Original Image</span>
+      <div id="original_image_container"></div>
+      <span>Compressed Image</span>
+      <div id="compressed_image_container"></div>
+
+      <button id="donwload">Download</button>
+  </div>
+
 </main>
 
-<style>
+
+<style> 
   
 </style>
